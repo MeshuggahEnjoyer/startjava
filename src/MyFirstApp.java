7fdd5d3f6766890fe32751091e8872d8351805f0
MeshuggahEnjoyer <acidspit8@gmail.com>
2024-02-28 14:51:20 +0300

    Добавил ввод имени участника

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class MyFirstApp {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in, "cp866");
+        System.out.print("Введите, пожалуйста, свое имя: ");
+        String name = console.next();
+
         System.out.println("У какого языка программирования следующий слоган:");
         System.out.print("\"Написано однажды, ");
         System.out.println("работает везде!\"");
 
-        String answer = new Scanner(System.in).next();
+        String answer = console.next();
         if (answer.equalsIgnoreCase("Java")) {
-            System.out.println("Вы угадали");
+            System.out.println(name + ", вы угадали!");
         } else System.out.println("Увы, но - это Java");
     }
 }
