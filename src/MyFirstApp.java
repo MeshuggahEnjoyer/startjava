a2a4a35ab58737826eb6ac569155263f11071a9c
MeshuggahEnjoyer <acidspit8@gmail.com>
2024-02-28 13:46:33 +0300

    Добавил quiz по слогану Java

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.print("Написано однажды, ");
-        System.out.println("работает везде!");
+        System.out.println("У какого языка программирования следующий слоган:");
+        System.out.print("\"Написано однажды, ");
+        System.out.println("работает везде!\"");
+
+        String answer = new Scanner(System.in).next();
+        if (answer.equalsIgnoreCase("Java")) {
+            System.out.println("Вы угадали");
+        } else System.out.println("Увы, но - это Java");
     }
 }
