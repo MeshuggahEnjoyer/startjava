a7b5ed404c3d992073faf02bf17b933a4d30bf2c
MeshuggahEnjoyer <acidspit8@gmail.com>
2024-03-29 18:21:28 +0300

    Задание по классам

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "male";
+    String firstName = "Alex";
+    float height = 1.80F;
+    float weight = 75.5F;
+    int age = 29;
+
+    void move(){
+        System.out.println("move");
+    }
+
+    void sit(){
+        System.out.println("sit");
+    }
+
+    void run(){
+        System.out.println("run");
+    }
+
+    void say(String phrase){
+        System.out.println(phrase);
+    }
+
+    void learnJava(){
+        System.out.println("learnJava");
+    }
+}
