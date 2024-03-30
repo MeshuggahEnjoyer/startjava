66af33d3ead22298b76d5071ae46a952d8975a85
MeshuggahEnjoyer <acidspit8@gmail.com>
2024-03-30 22:46:15 +0300

    Итоговое задание: Jaeger

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,84 @@
+public class Jaeger {
+    String modelName;
+    String mark;
+    float height;
+    float weight;
+    int strength;
+    int armor;
+
+    public Jaeger() {}
+
+    public Jaeger(String modelName, String mark, float height, float weight,
+            int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.height = height;
+        this.weight = weight;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void move() {
+        System.out.println(this.modelName + " идет");
+    }
+
+    public void scanKaiju() {
+        System.out.println(this.modelName + " сканирует кайдзю");
+    }
+
+    public void attack() {
+        if (this.modelName.equals("Cherno Alpha")) {
+            System.out.println(this.modelName + " активирует Incinerator turbines");
+        } else if (this.modelName.equals("Bracer Phoenix")) {
+            System.out.println(this.modelName + " активирует Vortex Cannon");
+        }
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,33 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger chernoAlpha = new Jaeger();
+        chernoAlpha.setModelName("Cherno Alpha");
+        chernoAlpha.setMark("Mark-1");
+        chernoAlpha.setHeight(85.34F);
+        chernoAlpha.setWeight(54.487F);
+        chernoAlpha.setStrength(10);
+        chernoAlpha.setArmor(10);
+
+        Jaeger bracerPhoenix = new Jaeger("Bracer Phoenix", "Mark-5", 70.91F, 48.072F, 8, 9);
+
+        chernoAlpha.move();
+        bracerPhoenix.move();
+
+        chernoAlpha.attack();
+        bracerPhoenix.attack();
+
+        chernoAlpha.scanKaiju();
+        bracerPhoenix.scanKaiju();
+
+        chernoAlpha.setModelName(bracerPhoenix.getModelName());
+        System.out.println("Новый modelName объекта chernoAlpha: " + chernoAlpha.getModelName());
+
+        bracerPhoenix.setModelName("Bracer Phoenix 2.0");
+        System.out.println("Новый modelName объекта bracerPhoenix: " + bracerPhoenix.getModelName());
+
+        bracerPhoenix.setWeight(chernoAlpha.getWeight());
+        chernoAlpha.setWeight(32.05F);
+        System.out.println("Новый weight объекта chernoAlpha: " + chernoAlpha.getWeight());
+        System.out.println("Новый weight объекта bracerPhoenix: " + bracerPhoenix.getWeight());
+    }
+}
