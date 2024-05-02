1ddf935204ab1286e38861786277dfa137951d62
MeshuggahEnjoyer <acidspit8@gmail.com>
2024-05-02 22:01:57 +0300

    Исправления по задачам с массивами

--- src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
@@ -5,90 +5,108 @@ import java.util.Scanner;
 
 public class ArraysTheme {
     public static void main(String[] args) {
+        reverseArray();
+        calculateFactorial();
+        resetArrayElements();
+        printLadderOfChars();
+        fillWithRandomNumbers();
+        hangMan();
+    }
+
+    private static void reverseArray() {
         System.out.println("1.Реверс значений массива");
         int[] numbers = {1, 7, 4, 5, 2, 6, 3};
         int length = numbers.length;
 
         System.out.print("До реверса: ");
-        printIntArray(numbers);
+        print(numbers);
 
-        int tmp = 0;
-        for (int i = 0; i < length / 2; i++) {
-            tmp = numbers[i];
-            numbers[i] = numbers[length - i - 1];
-            numbers[length - i - 1] = tmp;
+        for (int i = 0; i < length; i++) {
+            length--;
+            int swap = numbers[i];
+            numbers[i] = numbers[length];
+            numbers[length] = swap;
         }
 
         System.out.print("\nПосле реверса: ");
-        printIntArray(numbers);
+        print(numbers);
+        System.out.println();
+    }
 
-        System.out.println("\n\n2.Вычисление факториала");
-        int[] numbersZeroToNine = new int[10];
-        length = numbersZeroToNine.length;
+    private static void calculateFactorial() {
+        System.out.println("\n2.Вычисление факториала");
+        int[] multipliers = new int[10];
+        int length = multipliers.length;
 
-        //Заполнение массива
+        // Заполнение массива
         for (int i = 0; i < length; i++) {
-            numbersZeroToNine[i] = i;
+            multipliers[i] = i;
         }
 
-        //Подсчет факториала
         int factorial = 1;
-        for (int i = 2; i <= 8; i++) {
+        String resultOutput = "1";
+        for (int i = 2; i <= length - 2; i++) {
             factorial *= i;
+            resultOutput += (i != length - 2) ? (" * " + i) : (" * " + i + " = " + factorial);
         }
+        System.out.println(resultOutput);
+    }
 
-        //Вывод факториала
-        for (int i = 1; i < length - 1; i++) {
-            System.out.print(numbersZeroToNine[i] + (numbersZeroToNine[i] == 8 ? " = " + factorial : " * "));
-        }
+    private static void resetArrayElements() {
+        System.out.println("\n3.Удаление элементов массива");
+        double[] randomDoubles = new double[15];
+        int length = randomDoubles.length;
 
-        System.out.println("\n\n3.Удаление элементов массива");
-        double[] doubleNumbers = new double[15];
-        length = doubleNumbers.length;
-
-        //Заполнение массива
+        // Заполнение массива
         for (int i = 0; i < length; i++) {
-            doubleNumbers[i] = Math.random();
+            randomDoubles[i] = Math.random();
         }
-        printDoubleArray(doubleNumbers);
+        print(randomDoubles);
 
-        //изменение элементов массива
-        double middleNumber = doubleNumbers[length / 2];
+        // изменение элементов массива
+        double middleNumber = randomDoubles[length / 2];
+        int deletedNumbersAmount = 0;
         for (int i = 0; i < length; i++) {
-            if (doubleNumbers[i] <= middleNumber) {
-                continue;
+            if (randomDoubles[i] > middleNumber) {
+                randomDoubles[i] = 0;
+                deletedNumbersAmount++;
             }
-            doubleNumbers[i] = 0;
         }
-        printDoubleArray(doubleNumbers);
 
+        System.out.println();
+        print(randomDoubles);
+        System.out.printf("Количество обнуленных ячеек: %s%n", deletedNumbersAmount);
+    }
+
+    private static void printLadderOfChars() {
         System.out.println("\n4.Вывод алфавита лесенкой");
-        char[][] charsLadder = new char[26][];
-        length = charsLadder.length;
+        char[] chars = new char[26];
+        char currentChar = 65;
 
-        //Заполнение массива
-        for (int i = 0; i < length; i++) {
-            charsLadder[i] = new char[i+1];
-            char currentCharacter = 90;
-
-            for (int j = 0; j < charsLadder[i].length; j++) {
-                charsLadder[i][j] = currentCharacter--;
-            }
+        // Заполнение массива
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = currentChar;
+            currentChar++;
         }
 
-        //Вывод массива
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < charsLadder[i].length; j++) {
-                System.out.print(charsLadder[i][j]);
+        // Вывод лесенки
+        int currentRow = 0;
+        int lastCharacterIndex = chars.length - 1;
+        while (currentRow < 26) {
+            for (int i = lastCharacterIndex; i >= (lastCharacterIndex - currentRow); i--) {
+                System.out.printf("%s", chars[i]);
             }
+            currentRow++;
             System.out.println();
         }
+    }
 
+    private static void fillWithRandomNumbers() {
         System.out.println("\n5.Заполнение массива уникальными числами");
         int[] uniqueNumbers = new int[30];
-        length = uniqueNumbers.length;
+        int length = uniqueNumbers.length;
 
-        //Заполнение массива
+        // Заполнение массива
         for (int i = 0; i < length; i++) {
             boolean hasEqualNumber;
             do {
@@ -104,22 +122,23 @@ public class ArraysTheme {
             } while (hasEqualNumber);
 
         }
-        //Вывод массива
+        // Вывод массива
         Arrays.sort(uniqueNumbers);
-        printIntArray(uniqueNumbers);
+        print(uniqueNumbers);
+    }
 
+    private static void hangMan() {
         System.out.println("\n6.Игра “Виселица”");
         String[] words = {"Шахматы", "Игра", "Виселица", "Массив", "Геймпад"};
         String wordToGuess = words[(int) (Math.random() * 5)];
         char[] wordAsArray = wordToGuess.toCharArray();
         char[] mask = new char[wordAsArray.length];
 
-        //заполняем маску
-        System.out.println(wordToGuess);
+        // заполняем маску
         Arrays.fill(mask, '_');
-        printCharArray(mask);
+        print(mask);
 
-        //стартуем игру
+        // стартуем игру
         int attempts = 5;
         boolean gameFinished = false;
         char[] wrongletters = new char[20];
@@ -130,17 +149,17 @@ public class ArraysTheme {
             System.out.print("\nВведите букву: ");
             char guessedLetter = scanner.next().toCharArray()[0];
 
-            //если буква верная - заносим в маску
+            // если буква верная - заносим в маску
             for (int i = 0; i < wordAsArray.length; i++) {
                 if (Character.toLowerCase(wordAsArray[i]) == Character.toLowerCase(guessedLetter)) {
                     mask[i] = Character.toUpperCase(guessedLetter);
                     isWrongGuess = false;
                 }
             }
-            printCharArray(mask);
+            print(mask);
 
             if (isWrongGuess) {
-                //Записываем ошибочную букву в соотв. массив
+                // Записываем ошибочную букву в соотв. массив
                 for (int i = 0; i < wrongletters.length; i++) {
                     if (wrongletters[i] == '\u0000') {
                         wrongletters[i] = Character.toUpperCase(guessedLetter);
@@ -151,11 +170,11 @@ public class ArraysTheme {
                     }
                 }
             } else if (attempts < 5) {
-                    attempts++;
+                attempts++;
             }
 
             System.out.print("Ошибочные буквы: ");
-            printCharArray(wrongletters);
+            print(wrongletters);
             System.out.printf("Осталось попыток: %d%n", attempts);
             printGallows(attempts);
             gameFinished = !Arrays.toString(mask).contains("_");
@@ -169,38 +188,34 @@ public class ArraysTheme {
         }
     }
 
-    private static void printIntArray(int[] array) {
+    private static void print(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (i == array.length - 1) {
-                System.out.printf("%d]", array[i]);
-            } else if (i == 0) {
-                System.out.printf("[%d, ", array[i]);
+                System.out.printf("%d", array[i]);
             } else {
                 System.out.printf("%d, ", array[i]);
             }
             if ((i + 1) % 10 == 0) {
-                System.out.printf("%n ");
+                System.out.printf("%n");
             }
         }
     }
 
-    private static void printDoubleArray(double[] array) {
+    private static void print(double[] array) {
         for (int i = 0; i < array.length; i++) {
             if (i == array.length - 1) {
-                System.out.printf("%5.3f]%n", array[i]);
-            } else if (i == 0) {
-                System.out.printf("[%5.3f, ", array[i]);
+                System.out.printf("%5.3f%n", array[i]);
             } else {
                 System.out.printf("%5.3f, ", array[i]);
             }
 
             if (i == 7) {
-                System.out.printf("%n ");
+                System.out.printf("%n");
             }
         }
     }
 
-    private static void printCharArray(char[] array) {
+    private static void print(char[] array) {
         for (char c : array) {
             if (c != '\u0000') {
                 System.out.print(c + " ");
@@ -231,13 +246,12 @@ public class ArraysTheme {
                 System.out.println("        |");
                 System.out.println("        O");
                 System.out.println("       /|\\ ");
-                System.out.println("        |");
                 break;
             case 3:
                 System.out.println("__________");
                 System.out.println("        |");
                 System.out.println("        O");
-                System.out.println("       /|\\ ");
+                System.out.println("        |  ");
                 break;
             case 4:
                 System.out.println("__________");
